9523826741fb929ff0f9708685f7db01dff8a17d
bjekovicn <n.bjekovic97@gmail.com>
2023-08-16 22:13:40 +0200

    logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import {
   Box,
   Flex,
@@ -50,12 +49,6 @@ export default function WithSubnavigation() {
           />
         </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily={'heading'}
-            color={useColorModeValue('gray.800', 'white')}>
-            Logo
-          </Text>
 
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav />
