9398e6eb99216a9625777cb432affababc92e4fb
bjekovicn <n.bjekovic97@gmail.com>
2023-08-16 21:04:18 +0200

    header, footer, chakra

---
+++ src/components/Header.tsx
@@ -0,0 +1,6 @@
+
+const Header=()=>{
+    return (<div className="bg-green-dark h-20">Header</div>)
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx,html,css}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'green-dark': '#072114',
+      'green-light': '#075933',
+    },
   },
   plugins: [],
 }
