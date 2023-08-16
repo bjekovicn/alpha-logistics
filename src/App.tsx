fda4be7e2a3b057f962b31c3e8171b252886c5fe
bjekovicn <n.bjekovic97@gmail.com>
2023-08-16 20:31:38 +0200

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import "./App.css";
 
 function App() {
   return (
     <>
-      <h4>
+      <div className="bg-blue-600  p-10">
         Site is currently not available. The site is under construction and will
         be available from 01/07/2023. During this time you can email us at
         info@alfa-group.info. Kind regards.
-      </h4>
+      </div>
     </>
   );
 }
