31d83ee581f6bb9fc1955e390db762d40fc3fb56
bjekovicn <n.bjekovic97@gmail.com>
2023-08-21 08:54:09 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import { Heading, Highlight } from "@chakra-ui/react";
+import { Flex, Heading, Highlight } from "@chakra-ui/react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="bg-green-light">
-      <Header></Header>
-      <NavBar></NavBar>
-      <Heading
-        paddingTop={28}
-        paddingBottom={28}
-        lineHeight="tall"
-        textAlign="center"
+    <Flex flexDirection="column" minHeight="100vh">
+      <Header />
+      <NavBar />
+      <Flex
+        flex="1"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
       >
-        <Highlight
-          query={[" alphalogistic@gmail.com"]}
-          styles={{ px: "2", py: "1", rounded: "full", bg: "teal.100" }}
+        <Heading
+          paddingTop={28}
+          paddingBottom={28}
+          lineHeight="tall"
+          textAlign="center"
+          size="md"
         >
-          Site is currently not available. The site is under construction and
-          will be available soon. During this time you can email us at
-          alphalogistic@gmail.com
-        </Highlight>
-      </Heading>
-      <Footer></Footer>
-    </div>
+          <Highlight
+            query={[" alphalogistic@gmail.com"]}
+            styles={{ px: "4", py: "2", rounded: "full", bg: "teal.100" }}
+          >
+            Site is currently not available. The site is under construction and
+            will be available soon. During this time you can email us at
+            alphalogistic@gmail.com
+          </Highlight>
+        </Heading>
+      </Flex>
+      <Footer />
+    </Flex>
   );
 }
 
