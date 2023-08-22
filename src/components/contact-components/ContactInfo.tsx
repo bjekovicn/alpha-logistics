ba911c05a6638b599952f658dc23e80b1c7c0307
bjekovicn <n.bjekovic97@gmail.com>
2023-08-22 08:48:37 +0200

    contact info done

--- src/components/contact-components/ContactInfo.tsx
+++ src/components/contact-components/ContactInfo.tsx
@@ -40,26 +40,36 @@ const ContactInfo = () => {
   const subColor = useColorModeValue("gray.400", "gray.900");
 
   return (
-    <VStack p={{ sm: 5, md: 5, lg: 16 }} align="flex-start">
+    <VStack
+      p={{ sm: 5, md: 5, lg: 16 }}
+      align={{ base: "center", lg: "flex-start" }}
+    >
       <Heading color={color}>Contact Our Team</Heading>
       {contacts.map((contact, index) => (
-        <Flex key={index} align="start" direction={{ base: "column" }}>
-          <Text fontWeight="bold" color={color} mt={3}>
+        <Flex
+          key={index}
+          align={{ base: "center", lg: "flex-start" }} // Align center on small screens, flex-start on large screens
+          direction={{ base: "column" }}
+          justify="center"
+          textAlign="center"
+          mt={3}
+        >
+          <Text fontWeight="bold" color={color}>
             {contact.position}
           </Text>
           {contact.name && (
-            <Flex align="center" mr={2}>
+            <Flex align="center" mt={2}>
               <Text color={subColor}>{contact.name}</Text>
             </Flex>
           )}
           {contact.phone && (
-            <Flex align="center" mr={2}>
-              <Icon as={FaPhone} color={subColor} mr={3} />
+            <Flex align="center" mt={2}>
+              <Icon as={FaPhone} color={subColor} mr={2} />
               <Text color={subColor}>{contact.phone}</Text>
             </Flex>
           )}
-          <Flex align="center">
-            <Icon as={FaEnvelope} color={subColor} mr={3} />
+          <Flex align="center" mt={2}>
+            <Icon as={FaEnvelope} color={subColor} mr={2} />
             <Link href={`mailto:${contact.email}`} color={subColor}>
               {contact.email}
             </Link>
