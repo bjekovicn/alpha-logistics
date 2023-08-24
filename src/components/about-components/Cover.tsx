3d1084365a1504327422867cedf8f3e9da88fd50
bjekovicn <n.bjekovic97@gmail.com>
2023-08-24 14:58:41 +0200

    added photo covers

--- src/components/about-components/Cover.tsx
+++ src/components/about-components/Cover.tsx
@@ -9,6 +9,7 @@ import {
   Stack,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import MyImage from "../../assets/tanker2.jpg";
 
 export default function SplitWithImage() {
   const { t } = useTranslation();
@@ -42,11 +43,8 @@ export default function SplitWithImage() {
         </Stack>
         <Flex>
           <Image
-            rounded={"md"}
             alt={"feature image"}
-            src={
-              "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-            }
+            src={MyImage} // Use the imported image here
             objectFit={"cover"}
           />
         </Flex>
