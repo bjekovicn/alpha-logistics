2b8f98a5de722a1d9cd63b039882d755de4d13c0
bjekovicn <n.bjekovic97@gmail.com>
2023-09-27 11:54:58 +0200

    products page added

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,16 @@
+import { Flex } from "@chakra-ui/react";
+
+const ProductsPage = () => {
+  return (
+    <Flex
+      flex="1"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      Products
+    </Flex>
+  );
+};
+
+export default ProductsPage;
