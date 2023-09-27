95cb73860f40b4ff5aa35c8d4cddb8411383c1dd
bjekovicn <n.bjekovic97@gmail.com>
2023-09-27 15:49:12 +0200

    messae

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading, Highlight } from "@chakra-ui/react";
 
 const ProductsPage = () => {
   return (
@@ -8,7 +8,19 @@ const ProductsPage = () => {
       justifyContent="center"
       alignItems="center"
     >
-      Products
+      <Heading
+        paddingTop={22}
+        paddingBottom={22}
+        lineHeight="normal"
+        textAlign="center"
+      >
+        <Highlight
+          query={["products page"]}
+          styles={{ px: "4", py: "2", rounded: "full", bg: "teal.100" }}
+        >
+          The products page is under construction and will be available soon.
+        </Highlight>
+      </Heading>
     </Flex>
   );
 };
